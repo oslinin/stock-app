6427a00ef0527e99424131c15cfb5d570750f241
oslinin <oslinin@users.noreply.github.com>
2026-07-10 00:19:41 +0000

    deploy: 5eaa7e53485e20c6e65847e3beab3dc05f9564dd

---
+++ preview/claude/trading-platform-phase-2-aqlv4m/registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/stock-app/preview/claude/trading-platform-phase-2-aqlv4m/sw.js', { scope: '/stock-app/preview/claude/trading-platform-phase-2-aqlv4m/' })})}
